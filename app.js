4245b9e110694590ad2a0ad870082e8d0be389da
Dragos Adrian Pamfile <dragos.pamf@gmail.com>
2019-09-18 13:55:49 +0200

    App Hash

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -21,9 +19,6 @@ const userShema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-
-userShema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userShema);
 
@@ -42,7 +37,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err) {
         if (!err) {
@@ -55,7 +50,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, function(err, foundUser) {
         if (err) {
